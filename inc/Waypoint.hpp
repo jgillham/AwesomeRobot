583d3e46b2e508495459581535917e38d01cb302
Josh Gillham <usajoshgillham@gmail.com>
2012-12-10 20:44:04 -0700

    Documented Waypoint.

--- inc/Waypoint.hpp
+++ inc/Waypoint.hpp
@@ -1,10 +1,15 @@
 #ifndef WAYPOINT_STRUCTURE_INCLUDED
 #define WAYPOINT_STRUCTURE_INCLUDED
-
+/**
+ * Waypoint is just a simple data structure
+ *  to hold the next pose for the robot.
+ *
+ * @author Josh Gillham
+ * @version 12-9-12
+ */
 struct Waypoint {
   public:
+    /** Holds the angle and coordinate. */
     double theta, x, y;
 };
-
 #endif // WAYPOINT_STRUCTURE_INCLUDED
-
