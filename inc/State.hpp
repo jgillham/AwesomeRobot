399b4809e462ef1757a75bc30ab4beb38d2500c7
Josh Gillham <usajoshgillham@gmail.com>
2012-12-10 20:40:54 -0700

    Documented state.

--- inc/State.hpp
+++ inc/State.hpp
@@ -1,14 +1,23 @@
 #ifndef STATE_CLASS_INCLUDED
 #define STATE_CLASS_INCLUDED
 #include "FirstMate.hpp"
-
+/**
+ * State is an interface for any robot state. It has
+ *  methods that will be called by the first mate to
+ *  signal events.
+ *
+ * @author Josh Gillham
+ * @version 12-9-12
+ */
 class State {
   public:
+    /** Called when the state is getting started. */
     virtual void onBegin( FirstMate* firstMate ) = 0;
+    /** Called when the move has been completed. */
     virtual void onMove( FirstMate* firstMate ) = 0;
+    /** Called when the turn has been completed. */
     virtual void onTurn( FirstMate* firstMate ) = 0;
+    /** Called right before the state ends. */
     virtual void onEnd() = 0;
 };
-
 #endif // STATE_CLASS_INCLUDED
-
