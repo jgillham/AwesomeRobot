ccfeb0acab86814e728dd81f20e3b517481aeec1
Josh Gillham <usajoshgillham@gmail.com>
2012-12-11 09:47:02 -0700

    Documented GoState class methods.

--- src/GoState.cpp
+++ src/GoState.cpp
@@ -1,11 +1,26 @@
+/// Author: Josh Gillham
+/// Date: 12-9-12
+/// Description:
+///  Holds the methods for the GoState class.
+///
 #include "GoState.hpp"
 
-
+/**
+ * Initializes class variables.
+ *
+ * @param path is the list of Waypoints for the
+ *  robot to move through.
+ */
 GoState::GoState( std::vector< Waypoint* >* path ):
     path( path ) {
-
 }
 
+/**
+ * Called to get the state started. Tells the robot to
+ *  move towards the next point.
+ *
+ * @param firstMate the pointer to the First Mate.
+ */
 void GoState::onBegin( FirstMate* firstMate ) {
   if ( !this->path->empty() ) {
     Waypoint& next = *this->path->back();
@@ -15,16 +30,17 @@ void GoState::onBegin( FirstMate* firstMate ) {
   }
 }
 
-//bool needToTurn( FirstMate* firstMate, Waypoint* next ) {
-//  double pathTheta = firstMate->calculateTheta( next->x, next->y );
-//  double needTheta = pathTheta - firstMate->getHeading();
-//  if ( THETA_TOLORANCE < ABS( needTheta ) ) {
-//    firstMate->turn( pathTheta );
-//    return false;
-//  } else {
-//    firstMate->move( next->x, next->y );
-//  }
-//}
+/**
+ * Decides which action will be appropriate to bring the
+ *  robot closer to its target. It must choose between a
+ *  move and a turn. The first mate will give it solid
+ *  advice as to which move will be appropriate.
+ *
+ * @param firstMate the pointer to the firstMate.
+ *
+ * @return TRUE if the robot has reached the next waypoint
+ *  OR FALSE if the robot will be changing its pose.
+ */
 bool GoState::goToNextPoint( FirstMate* firstMate ) {
   if ( !this->path->empty() ) {
     Waypoint& next = *this->path->back();
@@ -39,21 +55,26 @@ bool GoState::goToNextPoint( FirstMate* firstMate ) {
   else return true;
   return false;
 }
-//  if ( !this->path->empty() ) {
-//    Waypoint* next = this->path->back();
-//    if ( needToTurn( firstMate, next ) ) {
-//      firstMate->turn( pathTheta );
-//    } else {
-//      firstMate->move( next->x, next->y );
-//    }
-//  }
-//}
+
+/**
+ * Called when the first mate thinks the turn has
+ *  been completed.
+ *
+ * @param firstMate the pointer to the first mate.
+ */
 void GoState::onTurn( FirstMate* firstMate ) {
   if ( !this->path->empty() ) {
     Waypoint& next = *this->path->back();
     firstMate->move( next );
   }
 }
+
+/**
+ * Called when the first mate thinks the move has
+ *  been completed.
+ *
+ * @param firstMate the pointer to the first mate.
+ */
 void GoState::onMove( FirstMate* firstMate ) {
   if ( !this->path->empty() ) {
     this->path->pop_back();
@@ -62,24 +83,11 @@ void GoState::onMove( FirstMate* firstMate ) {
     Waypoint& next = *this->path->back();
     firstMate->turn( next );
   }
-//  if ( !this->path->empty() ) {
-//    if ( this->goToNextPoint( firstMate ) ) {
-//      this->path->pop_back();
-//      this->onMove( firstMate );
-//    }
-//  }
 }
-//    if ( needToTurn( firstMate, next ) ) {
-//      firstMate->turn( pathTheta );
-//    } else {
-//      firstMate->move( next->x, next->y );
-//    }
-//    if ( this->arriveAtNext( firstMate, back ) ) {
-//      this->path->pop_back();
-//      this->onMove( firstMate );
-//    }
-//  }
-//}
-void GoState::onEnd() {
 
+/**
+ * Called right before the state ends.
+ */
+void GoState::onEnd() {
+  // Prepare for next state.
 }
