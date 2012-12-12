d4f190bf39f45fc976579c00da3d7012a9589d65
Josh Gillham <usajoshgillham@gmail.com>
2012-12-12 12:21:59 -0700

    Trying to get makefiles with arduinos to work.

---
+++ exp/makeTest/applet/makeTest.cpp
@@ -0,0 +1,32 @@
+#include "WProgram.h"
+#include <stdlib.h>
+extern "C" void __cxa_pure_virtual() { abort(); }
+int IRpin = 0;                                    // analog pin for reading the IR sensor
+
+void setup() {
+  Serial.begin(115200);                             // start the serial port
+}
+void loop() {
+  float volts = analogRead(IRpin)*0.0048828125;   // value from sensor * (5/1024) - if running 3.3.volts then change 5 to 3.3
+  float distance = 65*pow(volts, -1.10);          // worked out from graph 65 = theretical distance / (1/Volts)S - luckylarry.co.uk
+  int x = distance, multiplier=0;
+  
+  while (x > 255)
+  {    x = x-255;   multiplier++;  }
+  Serial.print( char(x) );
+  delay(100);
+  Serial.print( char(multiplier) );
+  delay(100);
+}
+int main(void)
+{
+	init();
+
+	setup();
+    
+	for (;;)
+		loop();
+        
+	return 0;
+}
+
