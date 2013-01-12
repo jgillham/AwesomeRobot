b4db97aa05ac9f1b77c441a4484cca60823d24c5
Josh Gillham <usajoshgillham@gmail.com>
2013-01-11 19:17:29 -0700

    Started design code to talk to Wicke's arm.

--- src/ld/Arm.h
+++ src/ld/Arm.h
@@ -1,6 +1,36 @@
 #ifndef ARM_CLASS_INCLUDED
 #define ARM_CLASS_INCLUDED
 
+#include <Servo.h>
+#define ARM_SERVOS 3
+#define ARM_DELTA
+#define ARM_SERVO_ID1  5
+#define ARM_SERVO_ID2  6
+#define ARM_SERVO_ID3  10
+
+class Arm {
+  private:
+    int newTheta[ARM_SERVOS];
+    int oldTheta[ARM_SERVOS];
+    Servo R[ARM_SERVOS]
+  public:
+    Arm( ): newTheta( {0,0,0} ), oldTheta( {0,0,0} ), delta( {0,0,0} ) {
+        R[0].attach(ARM_SERVO_ID1);
+        R[1].attach(ARM_SERVO_ID2);
+        R[2].attach(ARM_SERVO_ID3);
+    }
+    void setNewTheta( int num, int val ) {
+        newTheta[ num ] = val;
+    }
+    void moveAll() {
+        for( int i = 0; i < ARM_SERVOS; ++i ) {
+            if ( newTheta[ i ] != oldTheta[ i ] ) {
+
+            }
+        }
+    }
+
+};
 
 
 #endif // ARM_CLASS_INCLUDED
