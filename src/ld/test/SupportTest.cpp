f1d7444867066930ffa2614f11ca92c1353ddaba
Josh Gillham <usajoshgillham@gmail.com>
2013-01-13 02:44:50 -0700

    Wrote GB test for readMessage().

--- src/ld/test/SupportTest.cpp
+++ src/ld/test/SupportTest.cpp
@@ -4,16 +4,30 @@
 #include <stdio.h>
 // Testing macros.
 #include "tests.h"
+// For strlen()
+#include <string.h>
 
 // BEGIN Mock Declarations
 #include <malloc.h>
 void delay( int ) {
 }
 class HardwareSerial {
+  private:
+    const char* buffer;
+    int nextChar;
   public:
+    HardwareSerial( const char* buffer )
+        : buffer( buffer ), nextChar( 0 ) {
+    }
     char read() {
+        char ret = buffer[ nextChar ];
+        if ( ret ) {
+            ++nextChar;
+        }
+        return ret;
     }
     int available() {
+        return strlen( buffer + nextChar );
     }
     void write(char) {
     }
@@ -56,11 +70,98 @@ int total_tests = 0;
 bool goodBehavior_readMessage() {
   bool status = true;
   ++total_tests;
-  HardwareSerial serial;
-  StringBuilder message;
-  readMessage( serial, message );
+  // Check a typical message.
+  {
+      const char* expected = "Sample";
+      char input[50];
+      sprintf( input, ":%s;", expected );
+      HardwareSerial serial( input );
+      StringBuilder message;
+      TEST_(
+            readMessage( serial, message ),
+            printf( "FAILURE BECAUSE: readMessage should have found a message.\n" )
+      );
+      TEST_(
+            strcmp( message, expected ) == 0 && strlen( message ) == strlen( expected ),
+            printf( "FAILURE BECAUSE: Message length should be the same as \"%s\".\n", expected )
+      );
+  }
+  // Check a zero-length message
+  {
+      HardwareSerial serial( ":;");
+      StringBuilder message;
+      TEST_(
+            readMessage( serial, message ),
+            printf( "FAILURE BECAUSE: readMessage should have found a message.\n" )
+      );
+      TEST_(
+            strlen( message ) == 0,
+            printf( "FAILURE BECAUSE: Message should have been empty.\n" )
+      );
+  }
+  // Check a unterminated message.
+  {
+      const char* expected = "Sample";
+      char input[50];
+      sprintf( input, ":%s", expected );
+      HardwareSerial serial( input );
+      StringBuilder message;
+      TEST_(
+            !readMessage( serial, message ),
+            printf( "FAILURE BECAUSE: readMessage should have NOT found a message.\n" )
+      );
+      TEST_(
+            strcmp( message, expected ) == 0 && strlen( message ) == strlen( expected ),
+            printf( "FAILURE BECAUSE: Message should be \"%s\".\n", expected )
+      );
+  }
+  // Check message growth.
+  {
+      const char* expected = "Sample";
+      char input[50];
+      sprintf( input, ":%s;", expected );
+      StringBuilder message;
+      {
+          HardwareSerial serial( ":Sam");
 
-  TEST_( 0, printf( "FAILURE DUE TO: No tests yet.\n" ) );
+          TEST_(
+                !readMessage( serial, message ),
+                printf( "FAILURE BECAUSE: readMessage should have NOT found a message.\n" )
+          );
+          TEST_(
+                strlen( message ) == 3,
+                printf( "FAILURE BECAUSE: Message should be \"Sam\".\n" )
+          );
+      }
+      {
+          HardwareSerial serial( "ple;");
+
+          TEST_(
+                readMessage( serial, message ),
+                printf( "FAILURE BECAUSE: readMessage should have found a message.\n" )
+          );
+          TEST_(
+                strcmp( message, expected ) == 0 && strlen( message ) == strlen( expected ),
+                printf( "FAILURE BECAUSE: Message should be \"%s\" but was \"%s\".\n", expected, (const char*)message )
+          );
+      }
+  }
+  // Check a messed up message.
+  {
+      const char* expected = "amps";
+      char input[50];
+      sprintf( input, "Sa:%s;e", expected );
+      HardwareSerial serial( "Sa:amps;e");
+      StringBuilder message;
+      TEST_(
+            readMessage( serial, message ),
+            printf( "FAILURE BECAUSE: readMessage should have found a message.\n" )
+      );
+      TEST_(
+            strcmp( message, expected ) == 0 && strlen( message ) == strlen( expected ),
+            printf( "FAILURE BECAUSE: Message should be \"%s\".\n", expected )
+      );
+  }
 
   if ( status )
     printf( "Test %s successful!\n", __FUNCTION__ );
