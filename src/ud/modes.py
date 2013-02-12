4a519f5e412569bb5f874e6db09b84ca68346c83
Josh Gillham <usajoshgillham@gmail.com>
2013-02-12 08:08:58 -0700

    Replaced tabs with spaces.

--- src/ud/modes.py
+++ src/ud/modes.py
@@ -3,18 +3,18 @@ Module Tests:
 """
 
 class Mode:
-	"""
-	Class Tests:
-	>>> instance = Mode()
-	"""
-	def onMessageReceived( self, message ):
-		import settings
-		category = message[0]
-		print message
-		if category == settings.SERVICE_IRSENSOR_POLL:
-			self.onIRReadingReceived( utility.Reading( message ) )
-	def onIRReadingReceived( self, reading ):
-		pass
+    """
+    Class Tests:
+    >>> instance = Mode()
+    """
+    def onMessageReceived( self, message ):
+        import settings
+        category = message[0]
+        print message
+        if category == settings.SERVICE_IRSENSOR_POLL:
+            self.onIRReadingReceived( utility.Reading( message ) )
+    def onIRReadingReceived( self, reading ):
+        pass
 
 class Go( Mode ):
     """
@@ -46,24 +46,24 @@ class Grab( Mode ):
 
 # Only run when someone specifically runs this module.
 if __name__ == "__main__":
-	"""
-	This shows how to use the architecture to scan. First a 
-	 scan message is sent to start the scan. Next, each reading
-	 is sent to the mode. I would expect this code 
-	 to be modified to meet the needs of the team.
-	
-	I have noticed that the last message received is too small.
-	"""
-	# Needed libraries.
-	import utility
-	import settings
-	# Initialize the mode.
-	mode = Localize()
-	# Open the port and initialize contact.
-	messenger = utility.Messenger( utility.SerialPort() )
-	# Request the scan.
-	messenger.sendMessage( settings.SERVICE_IRSENSOR_POLL )
-	# Check for messages. Pass any messages to the mode.
-	while True:
-		if messenger.checkInBox():
-			mode.onMessageReceived( messenger.getMessage() )
+    """
+    This shows how to use the architecture to scan. First a 
+     scan message is sent to start the scan. Next, each reading
+     is sent to the mode. I would expect this code 
+     to be modified to meet the needs of the team.
+    
+    I have noticed that the last message received is too small.
+    """
+    # Needed libraries.
+    import utility
+    import settings
+    # Initialize the mode.
+    mode = Localize()
+    # Open the port and initialize contact.
+    messenger = utility.Messenger( utility.SerialPort() )
+    # Request the scan.
+    messenger.sendMessage( settings.SERVICE_IRSENSOR_POLL )
+    # Check for messages. Pass any messages to the mode.
+    while True:
+        if messenger.checkInBox():
+            mode.onMessageReceived( messenger.getMessage() )
